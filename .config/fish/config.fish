a52c5cf3060e996bb28c0766768bff1676d5db93
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2025-06-18 02:46:17 -0400

    Add nvibrant systemd autostart, rootless docker config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,4 +10,11 @@ if status is-interactive
     if type -q zoxide
         zoxide init --cmd cd fish | source
     end
+
+    # Someone else's skill issue
+    export PIP_BREAK_SYSTEM_PACKAGES=1
+
+    # https://wiki.archlinux.org/title/Docker#Rootless_Docker_daemon
+    set -x DOCKER_HOST unix://$XDG_RUNTIME_DIR/docker.sock
+
 end
--- .profile
+++ .profile
@@ -4,6 +4,3 @@ export PATH="$HOME/.local/bin:$PATH"
 
 # Disable annoying .python-history (https://stackoverflow.com/a/63653039)
 export PYTHONSTARTUP="$HOME/.config/python/startup.py"
-
-# Someone else's skill issue
-export PIP_BREAK_SYSTEM_PACKAGES=1
